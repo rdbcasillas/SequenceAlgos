f2d7d944065549751b065b287cd0b678dd100046
rdbcasillas <rdbcasillas11@gmail.com>
2017-05-22 11:58:14 -0500

    python code that calculates maximum kmer given DNA string

---
+++ maxkmer.py
@@ -0,0 +1,30 @@
+def findkmer(string, k):
+    stringfreq = {}
+    for i in range(0, len(string)):
+        tmp = string[i:i+k]
+        if tmp in stringfreq:
+            stringfreq[tmp] += 1
+        else:
+            stringfreq[tmp] = 1
+
+    highest = max(stringfreq.values())
+    maxstring = [key for key, v in stringfreq.items() if v == highest]
+    if highest == 1:
+        msg = "No most frequent " + str(k) + "mer found." \
+                "All are of length 1 :( "
+        return msg
+    else:
+        msg = "The most frequent " + str(k) + "mer" + \
+                " are " + ', '.join(maxstring) + \
+                ". They occur " + str(highest) + " times."
+        return msg
+
+
+input_str = raw_input("Paste the path of the string you would like to investigate."
+                  "Ideally same path as this program: ")
+kmer = input("What's the k in your kmer?")
+
+with open(input_str, 'r') as mystr:
+    content = mystr.read().replace('\n', '')
+
+print(findkmer(content, kmer))
